5ce050c15c4e6397743a2b53b9a677db5b01df6b
Wes Banyard <wesley.banyard@finconsgroup.com>
2024-02-26 09:26:32 +0100

    fix: email output

--- src/components/EmailOutput/EmailOutput.jsx
+++ src/components/EmailOutput/EmailOutput.jsx
@@ -1,36 +1,26 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 import { TextField } from '@mui/material';
 
-export default function EmailOutput({textFieldRef}) {
-  const {tasks, settings} = useSelector((state) => state);
-  const {emailHeader, emailFooter} = settings;
+export default function EmailOutput({ textFieldRef }) {
+  const { tasks, settings } = useSelector(state => state);
+  const { emailHeader, emailFooter } = settings;
 
-  const filterAndFormatTasks = (tasks) => {
+  const filterAndFormatTasks = tasks => {
     // Filter out tasks with completed: true
     const filteredTasks = tasks.filter(task => task.status !== 'closed');
-  
+
     // Format and modify tasks
     const formattedTasks = filteredTasks.map(task => {
-      let formattedString = `${task.taskTicketText} ${task?.emailNote && '- ('+ task.emailNote + ')'}`;
-  
-      // Add 4 spaces if status is not equal to "not started"
-      if (task.status === "waiting for info") {
-        formattedString = "            " + formattedString;
-      }
-
-      return formattedString;
+      return `${task.taskTicketText} ${task?.emailNote && '- (' + task.emailNote + ')'}`;
     });
 
-    const headerFooterTasks = [emailHeader, ...formattedTasks, emailFooter]
-  
+    const headerFooterTasks = [emailHeader, ...formattedTasks, emailFooter];
+
     // Concatenate formatted strings with line breaks
     const resultString = headerFooterTasks.join('\n');
-  
+
     return resultString;
   };
-  
- 
-  return (
-    <TextField disabled ref={textFieldRef} fullWidth value={filterAndFormatTasks(tasks)} placeholder='...'  multiline rows={4} />
-  )
+
+  return <TextField disabled ref={textFieldRef} fullWidth value={filterAndFormatTasks(tasks)} placeholder='...' multiline rows={4} />;
 }

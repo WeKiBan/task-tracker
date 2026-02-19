f9c8fabf07bb2f4b8d1de4ba1913a5bc98a40368
Wes Banyard <wesley.banyard@finconsgroup.com>
2026-02-19 18:11:12 +0100

    Fix GitHub Pages base-path routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -16,10 +16,14 @@ function Router() {
 }
 
 function App() {
+  const basePath = (import.meta.env.BASE_URL || "/").replace(/\/$/, "") || "/";
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Router />
+        <WouterRouter base={basePath}>
+          <AppRoutes />
+        </WouterRouter>
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>

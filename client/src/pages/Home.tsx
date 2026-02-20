ed036c5462a256ea0e00e6918b99b0f392c43208
Wes Banyard <wesley.banyard@finconsgroup.com>
2026-02-20 15:12:03 +0100

    get rid of imported tags

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -347,7 +347,7 @@ export default function Home() {
         title,
         status: "Not Started",
         note: "",
-        tags: ["imported"],
+        tags: [],
         projectIds: [],
       });
       toast({ title: `Imported ${jiraId}` });

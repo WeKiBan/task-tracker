7f9d3e9fa4552410e1074d55693a323fb8432f8b
Wes Banyard <wesley.banyard@finconsgroup.com>
2026-02-23 15:00:30 +0100

    add project added to window

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { useStore } from "@/hooks/use-store";
+
+// Expose addProject for browser console bulk import
+if (typeof window !== "undefined") {
+	window.addProject = useStore.getState().addProject;
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
